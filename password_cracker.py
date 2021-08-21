2ebcdb905bc592ef19d25f99320bb7d0979ea6d7
Daniel Mikes <max8@post.cz>
2021-08-21 12:52:45 +0200

    finished challenge

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,37 @@
 import hashlib
 
-def crack_sha1_hash(hash):
-    
+def crack(word):
+  enc_word = word.encode()
+  hash_obj = hashlib.sha1(enc_word)
+  hex_val = hash_obj.hexdigest()
+  return hex_val
+
+def crack_sha1_hash(hash, use_salts=False):
+  # open top-10000-passwords.txt:
+  with open('top-10000-passwords.txt','r') as file:
+    # read each password
+    for line in file:
+      word = line.rstrip()
+      if use_salts == False:
+        secret = crack(word)
+        # compare with hash
+        if secret == hash:
+          return word
+      elif use_salts == True:
+        with open('known-salts.txt','r') as salts:
+          # read each salt
+          for line in salts:
+            salt = line.rstrip()
+            # prepend salt
+            salt_word = salt + word
+            secret = crack(salt_word)
+            # compare with hash
+            if secret == hash:
+              return word
+            # append salt
+            word_salt = word + salt
+            secret = crack(word_salt)
+            if secret == hash:
+              return word
+    file.close()
+  return "PASSWORD NOT IN DATABASE"
